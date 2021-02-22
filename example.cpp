1ba73d5840a17331b6dd7f26bb4fa0cfc4334b6a
Sergey Koparov <sergeykoparov@gmail.com>
2021-02-22 17:54:19 +0300

    Update example to fit new implementation

--- example.cpp
+++ example.cpp
@@ -1,58 +1,64 @@
 #include "state_machine.h"
  
-enum class State{ Off, On, Updated };
-enum class Event{ Start, Update, Stop };
- 
-struct MyTransitionsInfo : state_machine::TransitionsInfoBase<State, Event>
-{
-    using Table = state_machine::TransitionTable
-    <
-        TransitionTo<State::On, FromStates<State::Off>, OnEvents<Event::Start>>,
-        TransitionTo<State::Off, FromStates<State::On, State::Updated>, OnEvents<Event::Stop>>,
-        TransitionTo<State::Updated, FromStates<State::On>, OnEvents<Event::Update>>
-    >;
-};
- 
-class Handler
+enum class MyState{ Off, On, Updated };
+enum class MyEvent{ Start, Restart, Update, UpdateRequeired, Stop };
+
+class MyHandler
 {
 public:
-	//  Other OnTransition/OnEnterState/OnLeaveState callbacks can be declared in a similar fashion if needed
+    //  Other OnTransition/OnEnterState/OnLeaveState callbacks can be declared in a similar fashion if needed
 
-    template<State From, State To, std::enable_if_t<From == State::Updated && To == State::Off>...>
+    template<MyState From, MyState To, std::enable_if_t<From == MyState::Updated && To == MyState::Off>...>
     void OnTransition()
     {
+        int i = 0; ++i;
         // Handle switch off from updated state
     }
- 
-    template<State S, std::enable_if_t<S == State::Updated>...>
+
+    template<MyState S, std::enable_if_t<S == MyState::Updated>...>
     void OnEnterState()
     {
+        int i = 0; ++i;
         // Handle first update
     }
- 
-    template<State S, std::enable_if_t<S == State::Off>...>
+
+    template<MyState S, std::enable_if_t<S == MyState::Off>...>
     void OnLeaveState()
     {
+        int i = 0; ++i;
         // Handle switch on
     }
 };
- 
+
+using MyStateMachine = cpsm::StateMachine<
+    cpsm::TransitionTable
+    <
+        cpsm::To<MyState::On,
+            cpsm::If<cpsm::From<MyState::Off>, cpsm::On<MyEvent::Start, MyEvent::Restart>>,
+            cpsm::If<cpsm::From<MyState::Updated>, cpsm::On<MyEvent::UpdateRequeired>>>,
+        cpsm::To<MyState::Updated,
+            cpsm::If<cpsm::From<MyState::On, MyState::Updated>, cpsm::On<MyEvent::Update>>>,
+        cpsm::To<MyState::Off,
+            cpsm::If<cpsm::From<MyState::On, MyState::Updated>, cpsm::On<MyEvent::Stop>>>
+    >,
+    MyHandler>;
+
 int main()
 {
-    Handler h;
-    state_machine::StateMachine<MyTransitionsInfo, Handler> machine{ h, State::Off };
-	
-	// does nothing as no appropriate transitions are found
-    machine.ProcessEvent<Event::Update>();
-	
-	// switches from Off to On, calls Handler::OnLeaveState<...> as an appropriate handler function is found	
-    machine.ProcessEvent<Event::Start>(); 
-	
-	// switches from On to Updated, calls Handler::OnEnterState<...> as an appropriate handler function is found	
-    machine.ProcessEvent<Event::Update>(); 
-	
-	// switched from Updated to Off, calls Handler::OnTransition<...> as an appropriate handler function is found	
-    machine.ProcessEvent<Event::Stop>();
-    
+    MyHandler h;
+    MyStateMachine machine{ h, MyState::Off };
+
+    // does nothing as no appropriate transitions are found
+    machine.ProcessEvent<MyEvent::Update>();
+
+    // switches from Off to On, calls Handler::OnLeaveState<...> as an appropriate handler function is found
+    machine.ProcessEvent<MyEvent::Start>();
+
+    // switches from On to Updated, calls Handler::OnEnterState<...> as an appropriate handler function is found
+    machine.ProcessEvent<MyEvent::Update>();
+
+    // switched from Updated to Off, calls Handler::OnTransition<...> as an appropriate handler function is found
+    machine.ProcessEvent<MyEvent::Stop>();
+
     return 0;
 }
